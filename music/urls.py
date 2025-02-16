007f763e2eab49fb4e7dc013f1d590cd5f603e02
Abhaysinh Deshmukh <abhysihdes031@gmail.com>
2025-02-16 14:12:20 +0530

    first commit

---
+++ music/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('core.urls')),
+    path('', include('accounts.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')), 
+
+    # API Urls
+    path('v1/api/', include([
+        path('', include('core.api.urls')),
+        path('accounts/', include('accounts.api.urls')),
+    ])),
+]
